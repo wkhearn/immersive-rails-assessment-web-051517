1cd0d9632a9518b0c95f9a25c157a6e9e76fd937
wkhearn <wkhearn@gmail.com>
2017-06-22 15:51:46 -0400

    added cards to dashboard; no experiences

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,4 +1,8 @@
 class Episode < ApplicationRecord
   has_many :appearances
   has_many :guests, through: :appearances
+
+  def number_and_date
+    "Episode ##{self.number} - #{self.date}"
+  end
 end
